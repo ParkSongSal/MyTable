95f22312a9316d0b2ac87568186793665403a4e6
psm <tkdanr2427@naver.com>
2023-12-28 17:48:51 +0900

    # Intro 화면 - 광고 제거

--- app/src/main/java/com/psm/mytable/ui/intro/IntroFragment.kt
+++ app/src/main/java/com/psm/mytable/ui/intro/IntroFragment.kt
@@ -9,25 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.google.android.gms.ads.AdError
-import com.google.android.gms.ads.FullScreenContentCallback
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.interstitial.InterstitialAd
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import com.psm.mytable.App
 import com.psm.mytable.EventObserver
-import com.psm.mytable.MainActivity
 import com.psm.mytable.databinding.FragmentIntroBinding
 import com.psm.mytable.ui.ingredients.IngredientsActivity
-import com.psm.mytable.utils.ToastUtils
 import com.psm.mytable.utils.getViewModelFactory
-import timber.log.Timber
 
 class IntroFragment: Fragment(){
     private lateinit var viewDataBinding: FragmentIntroBinding
     private val viewModel by viewModels<IntroViewModel> { getViewModelFactory() }
 
-    private var mInterstitialAd: InterstitialAd? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -51,7 +41,7 @@ class IntroFragment: Fragment(){
 
         Handler(Looper.getMainLooper()).postDelayed(
             {
-                initAd()
+                viewModel.appInit()
             },1500
         )
     }
@@ -70,77 +60,7 @@ class IntroFragment: Fragment(){
             val intent = Intent(context, IngredientsActivity::class.java)
             startActivity(intent)
             activity?.finish()
-            /*Handler(Looper.getMainLooper()).postDelayed(
-                {
-                    val intent = Intent(context, MainActivity::class.java)
-                    startActivity(intent)
-                    activity?.finish()
-                }, 1000
-            )*/
         })
-    }
-
-    private fun initAd(){
-        InterstitialAd.load(App.instance, "ca-app-pub-3145363349418895/5216668354", App.instance.adRequest, object : InterstitialAdLoadCallback(){
-            override fun onAdFailedToLoad(p0: LoadAdError) {
-                super.onAdFailedToLoad(p0)
-                mInterstitialAd = null
-                viewModel.appInit()
-            }
-
-            override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                super.onAdLoaded(interstitialAd)
-                mInterstitialAd = interstitialAd
-
-                if(mInterstitialAd != null){
-                    showInterstitial()
-                }else{
-                    viewModel.appInit()
-                    ToastUtils.showToast("Ad wasn`t loaded")
-                }
-
-            }
-        })
-    }
-
-    fun showInterstitial(){
-        if(mInterstitialAd != null){
-            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback(){
-                override fun onAdClicked() {
-                    super.onAdClicked()
-                    Timber.d("psm_onAdClikced")
-                }
-
-                // Called When ad is dismissed
-                override fun onAdDismissedFullScreenContent() {
-                    super.onAdDismissedFullScreenContent()
-                    Timber.d("psm_onAdDismissedFullScreenContent")
-                    mInterstitialAd = null
-                    viewModel.appInit()
-                }
-
-                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
-                    super.onAdFailedToShowFullScreenContent(p0)
-                    Timber.d("psm_onAdFailedToShowFullScreenContent")
-                    mInterstitialAd = null
-                }
-
-                override fun onAdImpression() {
-                    super.onAdImpression()
-                    Timber.d("psm_onAdImpression")
-                }
-
-                override fun onAdShowedFullScreenContent() {
-                    super.onAdShowedFullScreenContent()
-                    Timber.d("psm_onAdShowedFullScreenContent")
-                }
-
-            }
-            mInterstitialAd?.show(requireActivity())
-           // viewModel.appInit()
-        }else{
-            viewModel.appInit()
-        }
     }
 
     companion object {
